ca8bb8733ad2d80c1557efc66e31bb92dc02af9b
Pranta Saha <Pranta.dp@gmail.com>
2021-09-07 14:01:32 +0600

    Add files via upload

---
+++ Longest nondecresing subsequence.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector< pair<int,int> >LISseq;
+vector<int>parent(200005);
+
+int LISseqFunction(int arr[], int arrsz)
+{
+    if(arrsz > 0) {LISseq.push_back({arr[0],0}); parent[0]=-1;}
+
+    for(int i=1; i<arrsz; i++)
+    {
+        pair<int,int> a = {arr[i],-2}, b = {arr[i],1e9};
+        if(LISseq.back().first <= arr[i])
+        {
+            parent[i] = LISseq.back().second;
+            LISseq.push_back({arr[i],i});
+        }
+        else
+        {
+            pair<int,int> tmp = {arr[i],1e9};
+            auto it = upper_bound(LISseq.begin(),LISseq.end(),tmp);
+            if(it==LISseq.begin()) parent[i]=-1;
+            else
+            {
+                it--;
+                parent[i]=(*it).second;
+                it++;
+            }
+            tmp.second=i;
+            *it = tmp;
+        }
+    }
+    return LISseq.size();
+}
+
+///LISseq -1.1.0   0.1.1   1.1.2
+
+int main()
+{
+    int arr[] = { 1,1,1,1,1,5,5,5,1,10 };
+    cout<<LISseqFunction(arr,10)<<endl;
+    int indx = LISseq.back().second;
+    while(indx!=-1)
+    {
+        cout<<arr[indx]<<" ";
+        indx = parent[ indx ];
+    }
+    return 0;
+}
